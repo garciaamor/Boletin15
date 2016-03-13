1e7ae751b22411173f969c913cb13a642ca2cec1
Acer <Acer@192.168.0.12>
2016-03-13 17:14:45 +0100

    boletin15

---
+++ src/boletin15/persoas.java
@@ -0,0 +1,15 @@
+package boletin15;
+
+import javax.swing.JOptionPane;
+
+public class persoas {
+    int id,edad;
+    String nome,apelido;
+    
+    public void concentrarse(){
+        JOptionPane.showMessageDialog(null,"Concentrase a seleccion");
+      }
+    public void viaxar(){
+        JOptionPane.showMessageDialog(null,"Viaxa a selecion");
+    }
+}
